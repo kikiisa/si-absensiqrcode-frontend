9a351e9228065c94593e2d949d9450d0349c86db
kikiisa <kikiisa89@gmail.com>
2023-12-15 03:06:39 +0800

    fix

---
+++ src/components/Daftar.vue
@@ -0,0 +1,14 @@
+<script>
+</script>
+<template>
+  <main>
+    <div class="row justify-content-center">
+        
+    </div>
+  </main>
+</template>
+<style scoped>
+    .camera{
+        margin-top: 50px;
+    }
+</style>
